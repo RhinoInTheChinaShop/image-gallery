dca6c79630d9c61b789f1c80510bdfdbe635b6c7
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-05-29 20:26:39 -0400

    Added backbone of bootstrap

--- index.php
+++ index.php
@@ -3,5 +3,18 @@
 	 * index.php
 	 * Bootstraps installer if the gallery is not installed, otherwise bootstraps gallery.
 	 */
-
+	
+	/*
+	 * Fetch all settings
+	 */
+	require("settings.php");
+	
+	/*
+	 * If the gallery is not installed, redirect user to the install directory
+	 */
+	if(!$installed) {
+		header("Location: install/");
+		exit;
+	}
+	
 ?>
