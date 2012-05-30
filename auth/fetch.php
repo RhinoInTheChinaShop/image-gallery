7c17b2f15f921daaff120c3e105c72da48bf2ab2
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-05-30 15:13:07 -0400

    Switched from cookies to sessions

--- auth/fetch.php
+++ auth/fetch.php
@@ -3,16 +3,15 @@
 	 * Fetch authentication information about the user
 	 */
 	
-	if(isset($_COOKIE["auth"])) {
-		$username = sqlite_escape_string($_COOKIE["username"]);
-		$password = sqlite_escape_string($_COOKIE["password"]);
+	if(isset($_SESSION["auth"])) {
+		$username = sqlite_escape_string($_SESSION["username"]);
 		if(!sqlite_open($dbLocation, 0666, $dbError)) {
 			die("Error with the database: ".$dbError);
 		}
 		$query = sqlite_query("SELECT * FROM users WHERE username = '$username' LIMIT 2");
 		$user = sqlite_fetch_array($query);
-		if(!$user || $user["password"] != $password) {
-			die("User not found or wrong password");
+		if(!$user) {
+			die("User not found");
 		}
 		if(sqlite_fetch_array($query)) {
 			die("Multiple users with the same username.");
