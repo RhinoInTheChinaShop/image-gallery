93515b96f2d73b9529b0d230c7ac0d47bb36a562
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-05-30 08:40:09 -0400

    Added user login box to homepage

--- index.php
+++ index.php
@@ -25,11 +25,35 @@
 <html>
 	<head>
 		<title><?php echo parsePageTitle("Home"); ?></title>
-		<link rel="stylesheet" type="text/css" href="resources/page.css" />
-		<script type="text/javascript" src="resources/home.js"></script>
+		<link rel="stylesheet" type="text/css" href="{$rootLocation}resources/page.css" />
+		<script type="text/javascript" src="{$rootLocation}resources/home.js"></script>
 	</head>
 	<body>
 		<h1>Image Gallery Home</h1>
-		<?php echo $homepageDescription; ?>
+		<?php echo $homepageDescription; ?><hr />
+		<div class="left userLogin">
+			<?php
+				if($user) {
+					$realName = html_entity_decode($user["displayname"]);
+					$profilePicture = $user["profilepic"];
+					echo <<<EOD
+			<img src="{$rootLocation}/pictures/?picture=$profilePicture" class="left profilePicture" alt="$realName's profile picture" />
+			Logged in as $realName.<br />
+			<a href="{$rootLocation}auth/logout/" class="right">Logout</a>
+EOD;
+				}
+				else {
+					echo <<<EOD
+			<h4>Login</h4>
+			You are not logged in.  Login below to see private pictures, and get access to commenting, and many other features.<br />
+			<form action="{$rootLocation}auth/login/" method="POST">
+				Username: <input type="text" name="username" /><br />
+				Password: <input type="text" name="password" /><br />
+				<input type="submit" value="Login" />
+			</form>
+EOD;
+				}
+			?>
+		</div>
 	</body>
 </html>
