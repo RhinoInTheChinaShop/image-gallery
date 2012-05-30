9928e133435454c9b27644df0dfe057ba5ee1005
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-05-29 20:46:32 -0400

    Added temporary hardcoded settings file

---
+++ settings.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+	 * Settings file for the image gallery
+	 * Currently uses hardcoded settings,
+	 * but the installer will ask the user for most of these settings later.
+	 */
+	
+	$installed = true;
+?>
