e7cba4aabf7adf02f67eaf11d86e22f0ee7ae9f6
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-05-31 18:28:09 -0400

    Created regex sqlite query function

--- page.php
+++ page.php
@@ -8,3 +8,11 @@
 		$replacements[0] = $pageName;
 		return preg_filter($patterns, $replacements, $pageTitleTemplate);
 	}
+	
+	function sqlite_regex_query($database, $query, $error = null, $method = null) {
+		function my_sqlite_regexp($x,$y){
+			return (int)preg_match("`$y`i",$x);
+		}
+		sqlite_create_function($db, 'regexp', 'my_sqlite_regexp',2);
+		return sqlite_query($database, $query, $method, $error);
+	}
