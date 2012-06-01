d0f5275e9925d81b81817cdf611636b4afea44a2
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-06-01 10:58:57 -0400

    Added missing

--- auth/login.php
+++ auth/login.php
@@ -7,10 +7,10 @@
 	session_regenerate_id();
 	$username = sqlite_escape_string($_POST["username"]);
 	$password = sha1(sqlite_escape_string($_POST["password"]));
-	if(!sqlite_open($dbLocation, 0666, $dbError)) {
+	if(!$db = sqlite_open($dbLocation, 0666, $dbError)) {
 		die("Error with the database: ".$dbError);
 	}
-	$query = sqlite_query("SELECT * FROM users WHERE username = '$username' LIMIT 2");
+	$query = sqlite_query($db, "SELECT * FROM users WHERE username = '$username' LIMIT 2");
 	$user = sqlite_fetch_array($query);
 	if(!$user || $user["password"] != $password) {
 		die("User not found");
