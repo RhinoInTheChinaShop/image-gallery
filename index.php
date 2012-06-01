410ca4315e0afaf4e87d43ea88d4c8bff0a5c58b
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-06-01 18:07:33 -0400

    Added links for event page selection

--- index.php
+++ index.php
@@ -104,7 +104,9 @@ EOD;
 					$tempEventLimit = ($allowUserChangeEventLimit && isset($_GET["itemLimit"])) ? sqlite_escape_string($_GET["itemLimit"]) : sqlite_escape_string($eventLimit);
 					$tempOffset = (isset($_GET["page"])) ? sqlite_escape_string($tempEventLimit * $_GET["page"]) : 0;
 					$query = sqlite_regex_query($db, "SELECT * FROM events WHERE regexp(allowed_types, '$roles') LIMIT $tempEventLimit OFFSET $tempOffset");
+					$eventCount = 0;
 					while($event = sqlite_fetch_array($query)) {
+						$eventCount += 1;
 						$imageLink = htmlentities("{$rootLocation}albums/?album=".$event["id"]);
 						$thumbnailURL = htmlentities($event["thumbnailURL"]);
 						$text = htmlentities($event["name"]);
@@ -124,10 +126,10 @@ EOD;
 					 * TODO: make the currently selected option be selected
 					 * TODO: add javascript to submit the form after the number of items per page is selected
 					 */
+					$pageNumber = htmlentities($_GET["page"]);
 					if($allowUserChangeEventLimit) {
 						echo <<<EOD
 				<form name="eventLimit" method="GET">
-					<input type="hidden" name="page" value="$currentPage" />
 					Elements per page:
 						<select name="itemLimit">
 							<option value="5">5</option>
@@ -136,11 +138,42 @@ EOD;
 							<option value="50">50</option>
 							<option value="100">100</option>
 						</select>
+					<input type="hidden" name="page" value="$pageNumber" />
 					<input type="submit" value="Change item limit" />
 				</form>
 EOD;
 					}
-					// TODO: add page selection
+					/*
+					 * Page change links
+					 * TODO: add javascript to replace links with form submission (document.getElementsByClass, form.sumbit, etc)
+					 * TODO: add checks to only display links that have items on the page
+					 * TODO: add last page link
+					 */
+					$tempLimit = htmlentities($tempEventLimit);
+					$prevPage = $pageNumber-1;
+					$nextPage = $pageNumber+1;
+					$firstListedPage = ($pageNumber - 5 < 0) ? 0 : $pageNumber - 5;
+					echo <<<EOD
+				<form name="eventPage" method="GET">
+					<input type="hidden" name="itemLimit" value="$tempLimit" />
+					<input type="hidden" name="page" />
+					<a href="?page=0&itemLimit=$tempLimit" class="eventPageLink" data-page="0">First Page</a>
+EOD;
+					if($prevPage > -1) {
+						echo <<<EOD
+					<a href="?page={$prevPage}&itemLimit=$tempLimit" class="eventPageLink" data-page="$prevPage">Previous Page</a>
+EOD;
+					}
+					for($i = 0; $i < 16; $i++) {
+						$thisPageNumber = $firstListedPage + $i;
+						echo <<<EOD
+					<a href="?page={$thisPageNumber}&itemLimit=$tempLimit" class="eventPageLink" data-page="$thisPageNumber">$thisPageNumber</a>
+EOD;
+					}
+					echo <<<EOD
+					<a href="?page={$nextPage}&itemLimit=$tempLimit" class="eventPageLink" data-page="$thisPageNumber">Next Page</a>
+				</form>
+EOD;
 				?>
 			</div>
 		</div>
