52a9c80ac1526c087459bf03384f402fa6fac255
Ryan Leonard <hubforest.rhinoryan@spameater.org>
2012-06-09 14:49:23 -0400

    Draft of image importer, page framework/layout added

---
+++ import/event/index.php
@@ -0,0 +1,42 @@
+<?php
+	include("../../settings.php");
+	include("../../auth/fetch.php");
+	
+	/*
+	 * If the user is not allowed to import events, they are redirected to the homepage with an error.
+	*/
+	if(!$user && !$allowAnonImports) {
+		header("Location: {$rootLocation}?action=noanonimports");
+		exit;
+	}
+	if(!$allowAnonImports && !(in_array("*", $accountPermissions) || in_array("import.*", $accountPermissions) || in_array("import.event", $accountPermissions))) {
+		header("Location: {$rootLocation}?action=importeventpermerror");
+		exit;
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Import Event</title>
+		<script type="text/javascript" src="import.js"></script>
+		<link rel="stylesheet" type="text/css" href="import.css" />
+	</head>
+	<body>
+		<div id="settingsLink">
+			<!-- square-ish link to settings popup -->
+		</div>
+		<div id="helpSection">
+			<!-- help information -->
+		</div>
+		<div id="importedImages">
+			<!-- image import form -->
+			<!-- image thumbnail list -->
+		</div>
+		<div id="eventPlayground">
+			<!-- area to mess around with images -->
+		</div>
+		<div id="settingsOverlay">
+			<!-- full page overlay for settings -->
+		</div>
+	</body>
+</html>
